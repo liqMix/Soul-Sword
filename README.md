e67e0a6f1a2199a86f781b6d6d626da610975b5f
liqMix <liqmix@protonmail.com>
2021-04-10 22:16:33 -0700

    Removed Jailbase functionality

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ My contribution to a community learning exercise for building a RogueLike game.
 https://www.reddit.com/r/roguelikedev/
 
 # Requirements
-	tcod - https://pypi.org/project/tcod/
-	requests - https://pypi.org/project/requests/
-	numpy - https://pypi.org/project/numpy/
-	Pillow - https://pypi.org/project/Pillow/2.2.1/
-	simpleaudio - https://pypi.org/project/simpleaudio/
+    tcod==12.2.0
+    simpleaudio==1.0.4
+    requests==2.25.1
+    pillow==8.2.0
+    pyYaml==5.4.1
 	
 # Description
 I plan to use this project to explore multiple systems that I do not have much experience in.
@@ -44,30 +44,14 @@ I plan to use this project to explore multiple systems that I do not have much e
 # Disclaimer & Source
 All information used to seed entities are available as public records. 
 Out of respect for privacy, names are replaced and images are not stored.
-The ASCII conversion of images do not contain enough information to identify individuals.
+The ASCII conversion of images are abstract representations with insufficient information for identification.
 
 Any persons who do not consent to such use of their information as gathered from public records can contact me to be added to a blacklist.
 
 ## Clackamas County ##
-By not providing an API key, entities are sourced from the Clackamas County inmate roster API located here: https://web3.clackamas.us/roster/api/
+Entities are sourced from the publicly available Clackamas County inmate roster API located here: https://web3.clackamas.us/roster/api/
+
+All entities are presumed innocent until proven guilty in a court of law.
 
 This project is not affiliated in any capacity with Clackamas County.
 
-## Jailbase API ## 
-**Due to discrepancies between county records, this source is currently not supported, please run without providing a API key**
-### API Access ###
-An API key is required in order to access the full database. One can be acquired from https://rapidapi.com/jailbase/api/jailbase
-The key should be placed in a text file:
-<pre>data/key.txt</pre>
-If the file does not exist, an alternative reduced-size keyless source will be used instead.
-
-This project is not affiliated with JailBase.com.
-
-Inmate data provided by JailBase.com
- 
- Individuals are innocent until proven guilty in a court of law. 
- Data is believed to be reliable but is provided "as is". 
- Contact the appropriate governmental agency to verify.
- 
-Terms of Service:
- https://www.jailbase.com/api/terms/
--- src/api/load_inmates.py
+++ src/api/load_inmates.py
@@ -1,20 +1,8 @@
 import requests
-import random
 import json
-import time
 from .mugshot import Mugshot
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-'''
- Data provided by JailBase.com
- 
- Individuals are innocent until proven guilty in a court of law. 
- Data is believed to be reliable but is provided "as is". 
- Contact the appropriate governmental agency to verify.
- 
- Terms of Service:
-    https://www.jailbase.com/api/terms/
-'''
 
 
 class InmateList:
@@ -24,98 +12,10 @@ class InmateList:
         self.black_list = []
         self.inmate_list = []
         self.source_ids = []
-        if self.load_sources():
-            self.populate_inmates(num)
-        else:
-            self.alt_source_populate(num)
-
+        self.populate_inmates(num)
         self.replace_names()
 
-    def load_sources(self):
-        # Online
-        try:
-            response = requests.get("http://api.open-notify.org/iss-pass.json")
-
-        # Offline
-        except ConnectionError:
-            return
-
-        # Read in api key
-        try:
-            self.api_key = open(self.api_key).read()
-        except FileNotFoundError:
-            return False
-
-        # Read in sources
-        raw_sources = open(self.source_path).read().splitlines()
-        for source in raw_sources:
-            line = source.rsplit('\t')
-            self.source_ids.append(line[0])
-
-    def populate_inmates(self, number_of_inmates):
-        headers = {
-                   'X-RapidAPI-Host': 'jailbase-jailbase.p.rapidapi.com',
-                   'X-RapidAPI-Key': self.api_key
-                 }
-
-        count = 0
-        while count < number_of_inmates:
-            source_id = random.choice(self.source_ids)
-            try:
-                response = requests.get("https://jailbase-jailbase.p.rapidapi.com/recent/?source_id="
-                                        + source_id,
-                                        headers=headers).json()['records']
-
-                i = 0
-                if len(response) > 0:
-                    while i < 10 and i < len(response):
-                        if self.can_add(response[i]):
-                            break
-                        i += 1
-
-                    if i < 10 and i < len(response):
-                        response = response[i]
-                        inmate = {'name':    response['name'],
-                                  'mugshot': response['mugshot'],
-                                  'charges': response['charges'],
-                                  'source':  response['more_info_url']}
-
-                        self.inmate_list.append(inmate)
-                        count += 1
-
-                    # remove source from list
-                    else:
-                        self.source_ids.remove(source_id)
-
-            except json.decoder.JSONDecodeError:
-                pass
-
-            # API TOS is rate limited
-            time.sleep(1.25)
-
-        # Write changed source list back to file
-        with open('api/sources.txt', 'w') as file:
-            for source_id in self.source_ids:
-                file.write(source_id + '\n')
-
-    def can_add(self, inmate_to_add):
-        # Need charges to define enemy stats
-        if not inmate_to_add['charges']:
-            return False
-
-        # Only inmates with pictures
-        if inmate_to_add['mugshot'] == 'https://imgstore.jailbase.com/widgets/NoMug.gif':
-            return False
-
-        # If inmate already in list
-        for i in self.inmate_list:
-            if inmate_to_add['name'] == i['name']:
-                return False
-
-        return True
-
-    # Use an alternate source if API key is not found
-    def alt_source_populate(self, num):
+    def populate_inmates(self, num):
         response = requests.get("https://web3.clackamas.us/roster/extract/inmates", verify=False)
         inmates = json.loads(response.text)['results']
 
